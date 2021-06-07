967e331624ce185571a601f4fbc200a43d66db7b
l1npengtul <l1npengtul@protonmail.com>
2021-06-08 04:11:34 +0900

    add cargo definitions for windows only on windows binding crate

--- nokhwa-bindings-windows/build.rs
+++ nokhwa-bindings-windows/build.rs
@@ -1,5 +1,9 @@
+#[cfg(windows)]
 fn main() {
     windows::build!(
         Windows::Win32::Media::MediaFoundation::*
     )
 }
+
+#[cfg(not(windows))]
+fn main() {}
--- nokhwa-bindings-windows/src/lib.rs
+++ nokhwa-bindings-windows/src/lib.rs
@@ -1,1 +1,2 @@
+#[cfg(windows)]
 ::windows::include_bindings!();
